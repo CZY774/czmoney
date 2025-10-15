02daa843f82c14c40bd0838ac0c6eebf8ff719a2
CZY774 <yogaliuze@gmail.com>
2025-10-15 15:32:50 +0700

    refactor: Removes authenticated user redirection
    Simplifies the root page load function by removing the logic that redirects authenticated users to the dashboard.

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,10 +1,3 @@
-import { redirect } from '@sveltejs/kit';
-
-export async function load({ locals }) {
-  // If user is authenticated, redirect to dashboard
-  if (locals.user) {
-    throw redirect(302, '/dashboard');
-  }
-  
+export async function load() {
   return {};
 }
