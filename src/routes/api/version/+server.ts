6f21a26ade68b6534bc1dcf8f16bf59fee56b589
CZY774 <yogaliuze@gmail.com>
2026-06-24 16:24:05 +0700

    chore: update version endpoint to include build time from environment variables

--- src/routes/api/version/+server.ts
+++ src/routes/api/version/+server.ts
@@ -1,10 +1,13 @@
 import { json } from "@sveltejs/kit";
 import type { RequestHandler } from "./$types";
 
-export const GET: RequestHandler = async () => {
-  const version = process.env.npm_package_version || "1.1.7";
-  const buildTime = new Date().toISOString();
+const version = process.env.npm_package_version || "1.1.7";
+const buildTime =
+  process.env.VERCEL_GIT_COMMIT_SHA ||
+  process.env.VERCEL_DEPLOYMENT_ID ||
+  version;
 
+export const GET: RequestHandler = async () => {
   return json({
     version,
     buildTime,
